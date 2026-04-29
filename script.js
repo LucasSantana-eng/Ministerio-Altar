c82d682c23ee3a06aa184a3fc26af73415be16f3
Lucassantana-eng <m.duda180813@gmail.com>
2026-04-29 14:22:07 -0300

    ajuste manual de sincronização

--- script.js
+++ script.js
@@ -801,3 +801,5 @@ btnWhatsapp.addEventListener('click', sendToWhatsApp);
 btnStart.addEventListener('click', () => switchView('repertoire'));
 
 document.addEventListener('DOMContentLoaded', renderSingers);
+
+//teste
